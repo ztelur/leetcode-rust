f698629cfe6db973d9c2a7d7c8c5364539c1b4f1
Randy <ztelur@gmail.com>
2021-09-07 12:49:48 +0800

    update

--- src/p4_median_of_two_sorted_arrays.rs
+++ src/p4_median_of_two_sorted_arrays.rs
@@ -43,7 +43,6 @@ impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let first;
         let second;
-        // 小的为first，大的为second
         if nums1.len() <= nums2.len() {
             first = nums1;
             second = nums2;
@@ -62,13 +61,12 @@ impl Solution {
             part_x = (low + high) / 2;
             part_y = (first.len() + second.len() + 1) / 2 - part_x;
 
-            let one_left = if part_x == 0 { std::i32::MIN} else {first[part_x - 1]};
-            let one_right = if part_x == first.len() { std::i32::MAX } else { first[part_x]};
+            let one_left = if part_x == 0 { std::i32::MIN } else { first[part_x - 1] };
+            let one_right = if part_x == first.len() { std::i32::MAX } else { first[part_x] };
+            let two_left = if part_y == 0 { std::i32::MIN } else { second[part_y - 1] };
+            let two_right = if part_y == second.len() { std::i32::MAX } else { second[part_y] };
 
-            let two_left = if part_y == 0 { std::i32::MIN } else { second[part_y - 1]};
-            let two_right = if part_y == second.len() { std::i32::MAX } else { second[part_y]};
-
-            if one_left <= two_left && two_left <= one_right {
+            if one_left <= two_right && two_left <= one_right {
                 if (first.len() + second.len()) % 2 == 0 {
                     return f64::from(one_left.max(two_left) + one_right.min(two_right)) / 2.0;
                 } else {
@@ -77,7 +75,7 @@ impl Solution {
             } else if one_left > two_right {
                 high = part_x - 1;
             } else {
-                low  = part_x + 1;
+                low = part_x + 1;
             }
         }
         panic!("Wrong arrays");
