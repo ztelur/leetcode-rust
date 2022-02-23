0c25e185b54cdb5fde523743ad960546445f6481
Randy <ztelur@gmail.com>
2022-02-23 17:10:55 +0800

    update

--- src/java/p328_奇偶链表.java
+++ src/java/p328_奇偶链表.java
@@ -25,22 +25,44 @@ class Solution {
             return head;
         }
 
-        ListNode zeroNode = head; // 偶数指针
+        // 注意 head 是不会动的，可以直接返回的
+        ListNode preOneNode = head; // 偶数指针
         ListNode oneNode = head.next; // 奇数指针
 
-        ListNode curr = head;
-        int index = 0;
+        // [1,2,3,4,5]
+        // [0,1,0,1,0]
+
+        /**
+         *
+         * [1,2,3,4,5]
+         * 从 2 开始，它是基数，跳过 pre 和 one 分别指向 1 和 2
+         * 到了3，它是偶数，所以它被查到了 pre 的后边 整个数组变成了 [1,3,2,4,5] 此时，pre指向3，one指向2
+         * 到了4，它是基数，进行跳过
+         * 到了5，它是偶数，需要替换，插入到2的前边 [1,3,5,2,4]
+         */
+
+
+        // 从 index = 1 也就是 2 开始
+        ListNode curr = head.next;
+        int index = 1;
 
         while (curr != null) {
             if (index % 2 == 0) {
-                // 偶数
+                // 偶数 到了 3，需要插入到原来的 oneNode 前边
+                preOneNode.next = curr;
+                oneNode.next = curr.next;
+                curr.next = oneNode;
 
+                preOneNode = curr;
+                oneNode = curr.next;
+                curr = oneNode.next;
+                index ++;
             } else {
-                // 奇数
+                // 奇数 直接跳过 2 不动
+                index ++;
+                curr = curr.next;
             }
-
-
         }
-
+        return head;
     }
 }
